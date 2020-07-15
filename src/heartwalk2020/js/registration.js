41dcc3d3559376acd73ca95bbf57ebb1b1198ea2
Adam Kaye <akaye@charitydynamics.com>
2020-07-15 11:00:08 -0400

    polyfill for startswith in IE

--- src/heartwalk2020/js/registration.js
+++ src/heartwalk2020/js/registration.js
@@ -2068,3 +2068,12 @@
 
 
 })(jQuery);
+
+if (!String.prototype.startsWith) {
+  Object.defineProperty(String.prototype, 'startsWith', {
+      value: function(search, rawPos) {
+          var pos = rawPos > 0 ? rawPos|0 : 0;
+          return this.substring(pos, pos + search.length) === search;
+      }
+  });
+}
